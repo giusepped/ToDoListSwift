117e4d87495ec1430450e32d00d78f61d1aae838
giuseppe de santis <giusdesan@gmail.com>
2015-12-09 19:54:14 +0000

    linked tableView to main controller and adding every item to the tableview

---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,26 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Giuseppe De Santis on 09/12/15.
+//  Copyright © 2015 Giuseppe De Santis. All rights reserved.
+//
+
+import UIKit
+
+class ToDo: UITableViewCell {
+
+    @IBOutlet weak var toDoCellLabel: UILabel!
+     
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -8,24 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var toDoTextField: UITextField!
     
     var toDoArray : [String] = []
 
-    func addStringToArray(toDoString : String) { toDoArray.append(toDoString)
+    func addStringToArray(toDoString : String) { toDoArray.append(toDoString); tableView.reloadData()
     }
     
     @IBAction func saveButtonTapped(sender: AnyObject) {
         let toDoText = toDoTextField.text
         addStringToArray(toDoText!)
-        print(toDoArray)
     }
     
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        super.viewDidLoad(); tableView.delegate = self; tableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,6 +33,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // MARK: TableView DataSource and Delegate
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int { return 1 }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int { return toDoArray.count }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell { let cellIdentifer = "ToDo"; let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifer, forIndexPath: indexPath) as! ToDo; cell.toDoCellLabel.text = toDoArray[indexPath.row]; return cell }
 }
 
